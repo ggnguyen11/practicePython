43e2eb767119a97f573e97a1278b1060974fc57c
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-02-28 16:02:32 -0500

    Ex20 troubleshooting

--- practicePythonEx20.py
+++ practicePythonEx20.py
@@ -7,7 +7,7 @@ import random
 
 def gen_randlist():
     randlist = []
-    for i in range(10):
+    for i in range(9):
         randlist.append(random.randint(1, 9))
 # altering original randlist to be ordered
     randlist.sort()
@@ -28,26 +28,42 @@ def num_inlist():
 #print(num_inlist())
 
 # solution w/ binary search, list_halver() function to determine middle of
+# given list
 def list_halver(Lst):
     halved_list = round(len(Lst) / 2)
     return int(halved_list)
 
-# try pass variables through function or use global variables?
-def num_inlist_binary():
-    midpoint = list_halver(ordered_randlist)
-    median = ordered_randlist[midpoint]
-    list_half = []
-    num = int(input("I will randomly generate an ordered list of numbers.\n" \
-        + "Please provide a number to be checked if it is in the list.\n"))
-    if num < median:
-        list_half = ordered_randlist[0:midpoint]
-        midpoint = ordered_randlist[list_halver(list_half)]
-        return num_inlist_binary()
-    elif num > median:
-        list_half = ordered_randlist[midpoint:-1]
-        midpoint = ordered_randlist[list_halver(list_half)]
-        return num_inlist_binary()
-    elif num == median:
-        return True
+# initializing global variable to be modified by function below
+midpoint = list_halver(ordered_randlist)
+list_half = []
+# variable for given number to be checked via binary search
+binary_num = int(input("I will randomly generate an ordered list of numbers." \
+    + "\nPlease provide a number to be checked if it is in the list.\n"))
 
-#print(num_inlist_binary())
+# function implementing binary search, using median as a conditional
+def num_inlist_binary():
+    global midpoint
+    global list_half
+    median = ordered_randlist[midpoint]
+    if binary_num < median:
+        print(median)
+        #list_half = ordered_randlist[0:midpoint]
+        #halved = list_halver(list_half)
+        #midpoint = ordered_randlist[halved]
+        #return num_inlist_binary()
+    elif binary_num > median:
+        print(median)
+        #list_half = ordered_randlist[midpoint:-1]
+        #halved = list_halver(list_half)
+        #midpoint = ordered_randlist[halved]
+        #return num_inlist_binary()
+    elif binary_num == median:
+        print(str(binary_num) + " is within the generated list:\n" + \
+            str(ordered_randlist))
+        return False
+    else:
+        print(str(binary_num) + " is not within the generated list:\n" + \
+            str(ordered_randlist))
+        return False
+
+print(num_inlist_binary())

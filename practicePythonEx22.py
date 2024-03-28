7de43ba5b7af00be2a318f16dea54c04f7bb51e1
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-03-28 16:07:32 -0400

    ex22 progress

--- practicePythonEx22.py
+++ practicePythonEx22.py
@@ -4,9 +4,29 @@
 # extra: using a different .txt file (saved as ex22Extra.txt), count how
 # many of each "category" of each image there are. +string parsing
 
+names = []
 # opening a file for reading; like opening for writing but w/ different flag
 with open('ex22Base.txt', 'r') as open_file:
     line = open_file.readline()
     while line:
-        print(line)
+#        print(line)
+        if line not in names:
+            names.append(line.strip("\n"))
         line = open_file.readline()
+        
+uniqueNames = set(names)
+
+dictLength = len(uniqueNames)
+
+while dictLength != -1:
+    nameCount = {uniqueNames[dictLength - 1]:[], uniqueNames[dictLength - 2]:\
+        [], uniqueNames[dictLength - 3]:[]}
+
+print(nameCount)
+#for num in dictLength:
+#    print(num)
+    
+#for name in uniqueNames:
+#    nameCount[name].append(name)
+
+#print(nameCount)

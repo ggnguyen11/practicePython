d5675a7561cdab94c72233f08a77523ff808a21f
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-03 14:40:37 -0400

    ex22 completion

--- practicePythonEx22.py
+++ practicePythonEx22.py
@@ -4,29 +4,29 @@
 # extra: using a different .txt file (saved as ex22Extra.txt), count how
 # many of each "category" of each image there are. +string parsing
 
+# initializing names list and nameCount dictionary to be populated with file
+# data and number of name instances
 names = []
+nameCount = {"Darth":0, "Luke":0, "Lea":0}
+Darth = Luke = Lea = 0
 # opening a file for reading; like opening for writing but w/ different flag
 with open('ex22Base.txt', 'r') as open_file:
     line = open_file.readline()
     while line:
-#        print(line)
-        if line not in names:
+        if line.strip("\n") not in names:
             names.append(line.strip("\n"))
+# conditionals to increment name counters for each unique instance
+        if line.strip("\n") == "Darth":
+            Darth += 1
+        elif line.strip("\n") == "Luke":
+            Luke += 1
+        elif line.strip("\n") == "Lea":
+            Lea += 1
         line = open_file.readline()
-        
-uniqueNames = set(names)
 
-dictLength = len(uniqueNames)
-
-while dictLength != -1:
-    nameCount = {uniqueNames[dictLength - 1]:[], uniqueNames[dictLength - 2]:\
-        [], uniqueNames[dictLength - 3]:[]}
+# assigning values of name counters to dictionary pairs
+nameCount["Darth"], nameCount["Luke"], nameCount["Lea"] = Darth, Luke, Lea
 
 print(nameCount)
-#for num in dictLength:
-#    print(num)
-    
-#for name in uniqueNames:
-#    nameCount[name].append(name)
 
-#print(nameCount)
+# extra 

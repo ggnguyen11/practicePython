658f9fd532294abe413a7623785caf7024b3e8a2
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-12 16:17:26 -0400

    ex24 progress

--- practicePythonEx24.py
+++ practicePythonEx24.py
@@ -2,6 +2,17 @@
 # "pt 1 of 4 in tic tac toe" series, ask user for the size of the game board
 # and print that to the screen, using functions**
 
-print(" --- --- --- ")
-print("|   |   |   |")
-print(" --- --- --- ")
+#print(" --- --- --- ")
+#print("|   |   |   |")
+
+def dimensions():
+    rows = int(input("Let's make a game board!\n" + "Please specify its "\
+        + "dimensions.\n" + "Rows: "))
+    columns = int(input("Columns: "))
+    repeat = input("Please confirm if these are the correct dimensions:\n" + \
+        str(rows) + "x" + str(columns) + "\n")
+
+def hori_lines(rows):
+    pass
+
+dimensions()

4928500354f7f1538698e3e7b2940afe36b837af
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-15 16:16:40 -0400

    ex24 completion

--- practicePythonEx24.py
+++ practicePythonEx24.py
@@ -2,17 +2,31 @@
 # "pt 1 of 4 in tic tac toe" series, ask user for the size of the game board
 # and print that to the screen, using functions**
 
-#print(" --- --- --- ")
-#print("|   |   |   |")
+#
+def board(rows, columns):
+    horizontal = ' '
+    vertical = '|'
+    for n in range(columns):
+        horizontal += '--- '
+    for m in range(columns):
+        vertical += '   |'
+    for m in range(rows):
+        print(horizontal)
+        print(vertical)
+    print(horizontal)
 
 def dimensions():
     rows = int(input("Let's make a game board!\n" + "Please specify its "\
         + "dimensions.\n" + "Rows: "))
     columns = int(input("Columns: "))
     repeat = input("Please confirm if these are the correct dimensions:\n" + \
-        str(rows) + "x" + str(columns) + "\n")
-
-def hori_lines(rows):
-    pass
+        "\'y\' or \'n\'" + "\n" + str(rows) + "x" + str(columns) + "\n")
+    if repeat.lower() == 'y':
+        board(rows, columns)
+    elif repeat.lower() == 'n':
+        return dimensions()
+    else:
+        print("Please enter either \'y\' or \'n\' to confirm the dimensions")
+        return dimensions()
 
 dimensions()

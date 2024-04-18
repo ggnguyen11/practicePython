f19ccf00354b5b5b0f63453be5cc1daebd40ed8f
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-18 11:26:43 -0400

    ex25 completion

--- practicePythonEx25.py
+++ practicePythonEx25.py
@@ -6,19 +6,35 @@
 
 import random
 
-answer = int(input("Think of any number between 0 to 100 and I'll try to " + \
+answer = int(input("Think of any number between 0 to 100 and I will " + \
     "guess it: "))
 
+# initializing guess, count, lower and upper for while loop
 guess = random.randint(0, 100)
 count = 0
+lower = 0
+upper = 100
 
+# initial guess
 confirm = input("\nIs your number " + str(guess) + "?\n" + "Please enter " + \
     "\'y\' or \'n\'\n")
+count += 1
 
 while confirm.lower() == 'n':
-    deviation = input("Is your number higher or lower?\nPlease enter " + \
+    count += 1
+    deviation = input("\nIs your number higher or lower?\nPlease enter " + \
         "\'h\' or \'l\'\n")
+    if deviation == 'l':
+        upper = guess
+# -1 to upper bound to prevent redundancy
+        guess = random.randint(lower, upper - 1)
+    elif deviation == 'h':
+        lower = guess
+# + 1 to lower bound to prevent redundancy
+        guess = random.randint(lower + 1, upper)
     confirm = input("\nIs your number " + str(guess) + "?\n" + \
         "Please enter \'y\' or \'n\'\n")
-    if confirm.lower() == 'y':
-        print("Told you I could guess it.\nGuesses: " + str(count))
+
+if confirm.lower() == 'y':
+    print("\nTold you I could guess it.\nGuesses: " + str(count))
+#    print(guesses)

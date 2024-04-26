226c5a0f05c2e1011e97bbd71af26a5b34c1c660
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-26 15:29:54 -0400

    ex29 progress

--- practicePythonEx29.py
+++ practicePythonEx29.py
@@ -5,7 +5,17 @@
 # bonus: ask players if they want to play again and keep a running tally of
 # which player has won more
 
-# board function from exercise 24
+# initial game state matrix
+game = [[0, 0, 0], \
+        [0, 0, 0], \
+        [0, 0, 0]]
+
+# game start
+print("\nLet's play Tic Tac Toe!\nPlayer 1 (X) - your move.\n\n" + \
+        "Please enter the coordinates of the space where you'd like to " + \
+        "place a move.\n(Format: (row,col) with range (1,1) to (3,3))")
+
+# board() function from exercise 24
 def board(rows, columns):
     horizontal = ' '
     vertical = '|'
@@ -18,4 +28,25 @@ def board(rows, columns):
         print(vertical)
     print(horizontal)
 
+# player_moves() function from exercise 27, to determine placement of moves
+def player_moves(player):
+    while 0 in game[0] or 0 in game[1] or 0 in game[2]:
+        move = input("\nPlayer " + player + " move:\n").split(',')
+        if game[int(move[0]) - 1][int(move[1]) - 1] == 'X' or \
+        game[int(move[0]) - 1][int(move[1]) - 1] == 'O':
+            board()
+            print("That space is currently occupied, please enter another" + \
+                " set of coordinates.\n")
+            return player_moves(player)
+        else:
+            if player == 'X':
+                game[int(move[0]) - 1][int(move[1]) - 1] = 'X'
+                board()
+                player_moves('O')
+            elif player == 'O':
+                game[int(move[0]) - 1][int(move[1]) - 1] = 'O'
+                board()
+                player_moves('X')
+
+player_moves('X')
 board(3, 3)

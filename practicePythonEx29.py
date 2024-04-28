f24a2d304c80f3deb94db0ed47ac9658f735fe8f
ggnguyen11 <ggnguyen11@gmail.com>
2024-04-28 03:21:41 -0400

    ex29 progress

--- practicePythonEx29.py
+++ practicePythonEx29.py
@@ -6,38 +6,44 @@
 # which player has won more
 
 # initial game state matrix
-game = [[0, 0, 0], \
-        [0, 0, 0], \
-        [0, 0, 0]]
+game = [[' ', ' ', ' '], \
+        [' ', ' ', ' '], \
+        [' ', ' ', ' ']]
 
 # game start
 print("\nLet's play Tic Tac Toe!\nPlayer 1 (X) - your move.\n\n" + \
         "Please enter the coordinates of the space where you'd like to " + \
         "place a move.\n(Format: (row,col) with range (1,1) to (3,3))")
 
-# board() function from exercise 24
-def board(rows, columns):
-    horizontal = ' '
-    vertical = '|'
-    for n in range(columns):
-        horizontal += '--- '
-    for m in range(columns):
-        vertical += '   |'
-    for m in range(rows):
-        print(horizontal)
-        print(vertical)
-    print(horizontal)
+# board() function from exercise 24, refactored
+def board():
+    print(' --- --- --- ')
+    print('| '+ game[0][0] + ' |' + ' '+ game[0][1] + ' ' + '| '+ game[0][2] \
+    + ' |')
+    print(' --- --- --- ')
+    print('| '+ game[1][0] + ' |' + ' '+ game[1][1] + ' ' + '| '+ game[1][2] \
+    + ' |')
+    print(' --- --- --- ')
+    print('| '+ game[2][0] + ' |' + ' '+ game[2][1] + ' ' + '| '+ game[2][2] \
+    + ' |')
+    print(' --- --- --- ')
 
 # player_moves() function from exercise 27, to determine placement of moves
 def player_moves(player):
-    while 0 in game[0] or 0 in game[1] or 0 in game[2]:
-        move = input("\nPlayer " + player + " move:\n").split(',')
+    while ' ' in game[0] or ' ' in game[1] or ' ' in game[2]:
+        if player == 'X':
+# splits coordinates by excluding the comma, storing the two numbers in a list
+            move = input("\nPlayer 1's move:\n").split(',')
+        elif player == 'O':
+            move = input("\nPlayer 2's move:\n").split(',')
+# checking to see if space in matrix is occupied
         if game[int(move[0]) - 1][int(move[1]) - 1] == 'X' or \
         game[int(move[0]) - 1][int(move[1]) - 1] == 'O':
             board()
             print("That space is currently occupied, please enter another" + \
                 " set of coordinates.\n")
             return player_moves(player)
+# if free space
         else:
             if player == 'X':
                 game[int(move[0]) - 1][int(move[1]) - 1] = 'X'
@@ -47,6 +53,7 @@ def player_moves(player):
                 game[int(move[0]) - 1][int(move[1]) - 1] = 'O'
                 board()
                 player_moves('X')
+    # when all spaces are occupied
+    print("The game has ended. Player " + "winner" + " wins!")
 
 player_moves('X')
-board(3, 3)

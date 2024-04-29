14473e215cba58cfc97c35e541cc73a4dc2cff40
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-04-29 16:15:41 -0400

    ex29 progress

--- practicePythonEx29.py
+++ practicePythonEx29.py
@@ -5,10 +5,18 @@
 # bonus: ask players if they want to play again and keep a running tally of
 # which player has won more
 
+# function to reset game state
+def clear_board():
+    return([[' ', ' ', ' '], \
+            [' ', ' ', ' '], \
+            [' ', ' ', ' ']])
+
 # initial game state matrix
-game = [[' ', ' ', ' '], \
-        [' ', ' ', ' '], \
-        [' ', ' ', ' ']]
+game = clear_board()
+
+# initializing win count
+p1_wins = 0
+p2_wins = 0
 
 # game start
 print("\nLet's play Tic Tac Toe!\nPlayer 1 (X) - your move.\n\n" + \
@@ -28,9 +36,46 @@ def board():
     + ' |')
     print(' --- --- --- ')
 
+# function to replay game
+def repeat_game(winner):
+    repeat = input("The game has ended. " + winner + " wins!\nPlayer 1 " + \
+        "wins: " + str(p1_wins) + "\nPlayer 2 wins: " + str(p2_wins) + \
+        "Would you like to play again? (Y/N)\n")
+    # repeat game
+    return(repeat.lower())
+
 # player_moves() function from exercise 27, to determine placement of moves
 def player_moves(player):
     while ' ' in game[0] or ' ' in game[1] or ' ' in game[2]:
+# win conditions from ex26, modified
+        if (game[0][0] == 'X' and game[0][1] == 'X' and \
+        game[0][2] == 'X') or (game[0][0] == 'X' and \
+        game[1][0] == 'X' and game[2][0] == 'X') or \
+        (game[0][0] == 'X' and game[1][1] == 'X' and \
+        game[2][2] == 'X') or (game[0][2] == 'X' and \
+        game[1][2] == 'X' and game[2][2] == 'X') or \
+        (game[2][0] == 'X' and game[2][1] == 'X' and \
+        game[2][2] == 'X') or (game[0][2] == 'X' and \
+        game[1][1] == 'X' and game[2][0] == 'X') or \
+        (game[0][1] == 'X' and game[1][1] == 'X' and \
+        game[2][1] == 'X') or (game[1][0] == 'X' and \
+        game[1][1] == 'X' and game[1][2]) == 'X':
+            winner = 'Player 1'
+            return winner
+        elif (game[0][0] == 'O' and game[0][1] == 'O' and \
+        game[0][2] == 'O') or (game[0][0] == 'O' and \
+        game[1][0] == 'O' and game[2][0] == 'O') or \
+        (game[0][0] == 'O' and game[1][1] == 'O' and \
+        game[2][2] == 'O') or (game[0][2] == 'O' and \
+        game[1][2] == 'O' and game[2][2] == 'O') or \
+        (game[2][0] == 'O' and game[2][1] == 'O' and \
+        game[2][2] == 'O') or (game[0][2] == 'O' and \
+        game[1][1] == 'O' and game[2][0] == 'O') or \
+        (game[0][1] == 'O' and game[1][1] == 'O' and \
+        game[2][1] == 'O') or (game[1][0] == 'O' and \
+        game[1][1] == 'O' and game[1][2]) == 'O':
+            winner = 'Player 2'
+            return winner
         if player == 'X':
 # splits coordinates by excluding the comma, storing the two numbers in a list
             move = input("\nPlayer 1's move:\n").split(',')
@@ -54,6 +99,20 @@ def player_moves(player):
                 board()
                 player_moves('X')
     # when all spaces are occupied
-    print("The game has ended. Player " + "winner" + " wins!")
+    replay = input("There are no winners for this game state.\n" + \
+    "Player 1 wins: " + str(p1_wins) + "\nPlayer 2 wins: " + \
+    str(p2_wins) + "\nPlay again? (Y\N)\n")
+    if replay.lower() == 'y':
+        player_moves('X')
+    elif replay.lower() == 'n':
+        pass
 
-player_moves('X')
+game_winner = player_moves('X')
+
+# updating win count
+if game_winner == 'Player 1':
+    p1_wins += 1
+    repeat_game(game_winner)
+elif game_winner == 'Player 2':
+    p2_wins += 1
+    repeat_game(game_winner)

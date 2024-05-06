a35ab4a1b85eace5c62c341a9ff5ac4065253d52
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-05-06 16:10:57 -0400

    ex31 progress

--- practicePythonEx31.py
+++ practicePythonEx31.py
@@ -6,7 +6,12 @@
 # extra: keep track of letters guessed and display a different message
 # for duplicate guesses
 
+# notes: try to use a dictionary to store and access sample letters
+# instead of unordered lists/sets
+
 sample = 'EVAPORATE'
+guessedLetters = []
+count = 0
 
 # takes the word and appends its characters to a list one by one
 def speller(word):
@@ -14,18 +19,36 @@ def speller(word):
     letters = []
     for char in range(length):
         letters.append(word[char])
+    return letters
 
 # displays missing spaces for each character in the given word
 def guess_bar(word):
-    bar = [' ']
+    bar = []
     for char in word:
-        bar.append('_ ')
-    print(bar)
+        bar.append('_')
     return bar
 
-def matcher():
-    pass
+#
+def matcher(count):
+    while '_' in bar:
+        guess = input("Please guess a letter:\n")
+        if guess.upper() in correctLetters and guess not in guessedLetters:
+            count += 1
+            guessedLetters.append(guess)
+            print("Guesses: " + str(count))
+            printBar(guessedLetters)
+        elif guess in guessedLetters:
+            count += 1
+            print("That letter has already been guessed.\n" + "Guesses: " + \
+            str(count) + "\n")
+            return matcher(count)
 
-speller(sample)
-guess_bar(sample)
-# matcher(guess_bar(sample))
+def printBar(guesses):
+    for char in guesses:
+        if char in correctLetters:
+            pass
+
+letters = speller(sample)
+correctLetters = set(letters)
+bar = guess_bar(sample)
+matcher(count)

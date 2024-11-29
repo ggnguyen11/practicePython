4559cecb35297a4ffb0ac29ff516ff6154e57968
ggnguyen11 <157182298+ggnguyen11@users.noreply.github.com>
2024-11-29 14:09:39 -0500

    sheetMaker.py progress

--- sheetMaker.py
+++ sheetMaker.py
@@ -10,6 +10,18 @@ sheet = workbook.active
 # writes data to sheet
 #sheet.append(data)
 
-# saves workbook
-workbook.save('test_sheetMaker.xlsx')
+# function to name workbook object
+def name_sheet():
+    while True:
+        workbook_name = input("\nEnter spreadsheet name:\n")
+        confirmation = input(f'\nIs this correct?' + \
+            f' (Enter Y/N)\n{workbook_name}.xlsx\n')
+        if confirmation.lower() == 'y' or confirmation[0] == 'y':
+            break
+    return workbook_name
 
+# assigns function return value to global variable
+workbook_name = name_sheet()
+
+# saves workbook with .xlsx file extension
+workbook.save(f'{workbook_name}.xlsx')
